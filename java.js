2e4e07064041f7e6c9c678240cd0846475b661e5
Lukas <67201471+LukasKuzel@users.noreply.github.com>
2021-01-13 17:13:56 +0100

    Flappy-Vit 2.0

--- java.js
+++ java.js
@@ -1,10 +1,51 @@
 let block = document.getElementById("block")
 let hole = document.getElementById("hole")
+let character = document.getElementById("character")
+let jumping = 0;
+let counter = 0;
 
 /*Pokaždé, když se spustí tato funkce, tak s ní i animace */ 
 hole.addEventListener('animationiteration', () => {
-    let random = -((Math.random()*400)+200);
+    let random = -((Math.random()*400)+250);
     hole.style.top = random + "px";
+    counter++;
 });
 
+setInterval(function(){
+    let characterTop = 
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    if(jumping==0){
+        character.style.top = (characterTop+3)+"px";
+    }
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    let holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    let cTop = -(700-characterTop);
+    if((characterTop>680)||((blockLeft<61)&&(blockLeft>-150)&&((cTop<holeTop)||(cTop>holeTop+160)))){
+        if (confirm("Game is over. Score: "+counter+" Try again?")) {
+            location.reload();
+          } else {
+            alert ("Thanks for playing.");
+            close();
+          }
+        character.style.top = 100 + "px";  
+        counter=0;
+    }
+},10)
 
+function jump(){  
+    jumping = 1;
+    let jumpCount = 0;
+    let jumpInterval = setInterval(function(){
+        let characterTop = 
+        parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        if((characterTop>6)&&(counter<15)){
+            character.style.top = (characterTop-5)+"px";
+        }
+        if(jumpCount>20){
+            clearInterval(jumpInterval);
+            jumping=0;
+            jumpCount=0;
+        }
+        jumpCount++;
+    },10);  
+}  
